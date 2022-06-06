c5f0988ddb8898c1de4ee3a39b4122792706c23d
danielkarl888 <danielkarl888@gmail.com>
2022-06-06 10:55:59 +0300

    added message class

---
+++ app/src/main/java/com/example/wappbiu_android/entities/Message.java
@@ -0,0 +1,52 @@
+package com.example.wappbiu_android.entities;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.Date;
+
+@Entity
+public class Message {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String content;
+
+    private Date created;
+
+    private boolean sent;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public boolean isSent() {
+        return sent;
+    }
+
+    public void setSent(boolean sent) {
+        this.sent = sent;
+    }
+
+
+}
