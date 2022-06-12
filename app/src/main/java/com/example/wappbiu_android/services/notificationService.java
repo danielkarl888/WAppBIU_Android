2951889c822cff80deaa8e175fd1a028698835a8
dvirlan <dvir37@gmail.com>
2022-06-12 12:19:23 +0300

    gradle and manifest update

--- app/src/main/java/com/example/wappbiu_android/services/notificationService.java
+++ app/src/main/java/com/example/wappbiu_android/services/notificationService.java
@@ -4,12 +4,19 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+import androidx.annotation.NonNull;
+
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 public class notificationService extends FirebaseMessagingService {
     public notificationService() {
     }
 
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
 }
